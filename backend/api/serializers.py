13311f06e53c3215fd3018f6b909cc7e8ff7762c
LiYiTing04 <lyting987@gmail.com>
2025-08-15 18:41:16 +0800

    MeetingSetup can save into db

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.utils import timezone
 from .models import Task, BusinessData, ProcessedReport, Meeting, Employee,Department
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
@@ -29,4 +30,24 @@ class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
         fields = '__all__'
+        
+class MeetingSubmitSerializer(serializers.ModelSerializer):
+    created_at_local = serializers.SerializerMethodField()
+    updated_at_local = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Meeting
+        fields = '__all__'
+        read_only_fields = ['meeting_id', 'created_at', 'updated_at']  # prevent POST from overwriting timestamps
+
+    def get_created_at_local(self, obj):
+        if obj.created_at:
+            # Convert UTC to local timezone (settings.TIME_ZONE)
+            return timezone.localtime(obj.created_at).strftime('%Y-%m-%d %H:%M:%S')
+        return None
+
+    def get_updated_at_local(self, obj):
+        if obj.updated_at:
+            return timezone.localtime(obj.updated_at).strftime('%Y-%m-%d %H:%M:%S')
+        return None
 
