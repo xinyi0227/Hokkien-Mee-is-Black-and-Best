8d78486d0a820b0c84fea6e1d1c0ac38ebabd775
LauYongPin <yongpinlau8@gmail.com>
2025-08-25 21:35:21 +0800

    profile

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabase";
 import Header from "./header";
 
-const Details = () => {
+const Profile = () => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -11,18 +11,15 @@ const Details = () => {
       if (!email) return;
 
       try {
-      const { data, error } = await supabase
-        .from("employee")
-        .select(`
-          email,
-          role,
-          department:employee_department_id_fkey ( department_name )
-        `)
-        .eq("email", email)
-        .single();
-
-        //console.log("data:", data);
-        //console.log("error:", error);
+        const { data, error } = await supabase
+          .from("employee")
+          .select(`
+            email,
+            role,
+            department:employee_department_id_fkey ( department_name )
+          `)
+          .eq("email", email)
+          .single();
 
         if (error) {
           console.error(error);
@@ -42,13 +39,13 @@ const Details = () => {
     fetchUser();
   }, []);
 
-  if (!user) return <div className="p-6">Loading user details...</div>;
+  if (!user) return <div className="p-6">Loading profile...</div>;
 
   return (
     <>
       <Header />
       <div className="max-w-md mx-auto p-6 bg-white shadow rounded mt-6">
-        <h2 className="text-2xl font-bold mb-4">User Details</h2>
+        <h2 className="text-2xl font-bold mb-4">Profile</h2>
         <p><strong>Email:</strong> {user.email}</p>
         <p><strong>Role:</strong> {user.role}</p>
         <p><strong>Department:</strong> {user.department_name}</p>
@@ -57,4 +54,4 @@ const Details = () => {
   );
 };
 
-export default Details;
+export default Profile;
